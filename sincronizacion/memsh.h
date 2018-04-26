5b9f5c076356303ca309cdab66be56b5032bd4c2
anth0 <antho.montero>
2018-04-26 21:09:58 +0000

    Conflicto faltante

--- sincronizacion/memsh.h
+++ sincronizacion/memsh.h
@@ -7,11 +7,7 @@
 
 #define ID 999
 #define ROUTE "/bin/lspci"
-<<<<<<< HEAD
-#define SEM_ID 0x000000
-=======
 #define SEM_ID 0x123456
->>>>>>> 489a1666eab16cc6bbb647a4d8a7282b8c72d7c2
 
 typedef struct
 {
@@ -22,9 +18,5 @@ typedef struct
 
 typedef struct{
   pid_t pid_mon;
-<<<<<<< HEAD
-	inf_p array_p[30];
-=======
-	inf_p array_p[20];
->>>>>>> 489a1666eab16cc6bbb647a4d8a7282b8c72d7c2
+inf_p array_p[20];
 }shmem_data;
